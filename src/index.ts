745d5d88ca52185c40aa874ea8c6b39b5955ba9b
Richie Bendall <richiebendall@gmail.com>
2019-04-13 19:57:40 +1200

    Update code

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 /**
  * @license
- * 
+ *
  * MIT License
  *
  * Copyright (c) 2019 Richie Bendall
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,83 +23,87 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-export function consultor() {
-    function css(value: any, styles: string) {
+
+class consultor {
+    css(value: any, styles: string) {
         console.log(`%c${value}`, styles)
     }
 
-    function log(value: any) {
+    log(value: any) {
         console.log(value)
     }
 
-    function obj(ob: object) {
+    obj(ob: object) {
         console.log(JSON.parse(JSON.stringify(ob)));
     }
 
-    function inf(el: any) {
+    inf(el: any) {
         console.dir(el)
     }
 
-    function err(value: any, code = null) {
+    err(value: any, code = "") {
         console.error(value, code)
     }
 
-    function war(value: any, code = null) {
+    war(value: any, code = "") {
         console.warn(value, code)
     }
 
-    function equ(exp: any, msg: string) {
+    equ(exp: any, msg: string) {
         console.assert(exp, msg)
     }
 
-    function cls() {
+    cls() {
         console.clear()
     }
 
-    function cnt(name = null) {
-        console.count(name)
+    cnt(label = "default") {
+        console.count(label)
     }
 
-    function cnr(name = null) {
-        console.countReset(name);
+    cnr(label = "default") {
+        // @ts-ignore: Property 'countReset' does not exist on type 'Console'.
+        console.countReset(label);
     }
 
-    function grp(name: string, ob: any, collapsed = false) {
-        if (collapsed) console.groupCollapsed(name);
-        else console.group(name);
+    grp(label: string, ob: object, collapsed = false) {
+        if (collapsed) console.groupCollapsed(label);
+        else console.group(label);
         Object.entries(ob).forEach(i => {
             console.log(i[0], i[1]);
         })
         console.groupEnd();
     }
 
-    function prf(name = null, func: any) {
-        console.profile(name);
+    prf(label = "default", func: any) {
+        console.profile(label);
         func()
         console.profileEnd();
     }
 
-    function tbl(ob: object) {
+    tbl(ob: object) {
         console.table(ob)
     }
 
-    function tme(name = null, func: any) {
-        console.time();
+    tme(label = "default", func: any) {
+        console.time(label);
         func()
-        console.timeEnd();
+        console.timeEnd(label);
     }
 
-    function tms(name = null) {
-        console.timeStamp(name)
+    tms(label = "default") {
+        console.timeStamp(label)
     }
 
-    function trc() {
+    trc() {
         console.trace();
     }
 
-    function vre(variable: any) {
+    vre(variable: any) {
         console.log({
             variable
         })
     }
 }
+
+export default new consultor();
